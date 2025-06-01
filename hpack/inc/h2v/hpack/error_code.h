412ca76dce1f230d19d24c3a94a3eae9a68f3692
lwdjohari <linggawasistha.djohari@outlook.com>
2025-06-02 00:17:03 +0700

    Add embedded metada for h2v::hpack

--- hpack/inc/h2v/hpack/error_code.h
+++ hpack/inc/h2v/hpack/error_code.h
@@ -1,10 +1,37 @@
 #pragma once
 #include <cstddef>
 #include <cstdint>
+
+extern "C" inline const char H2V_HPACK_VERSION[]
+    __attribute__((used, visibility("default"))) = "H2V_HPACK_VERSION: v0.6.1";
+
+#if defined(H2V_HPACK_HUFFMAN_ENCODER_USE_BIT_OP) && \
+    (H2V_HPACK_HUFFMAN_ENCODER_USE_BIT_OP == 1)
+extern "C" inline const char H2V_HPACK_HUFFMAN_ENCODER[]
+    __attribute__((used, visibility("default"))) =
+        "H2V_HPACK_HUFFMAN_ENCODER: 0x1 - Bits operations";
+#else
+extern "C" inline const char H2V_HPACK_HUFFMAN_ENCODER[]
+    __attribute__((used, visibility("default"))) =
+        "H2V_HPACK_HUFFMAN_ENCODER: 0x0 - Precomputed Encode Table";
+#endif
+
+#if defined(H2V_HPACK_HUFFMAN_DECODER_USE_FULLBYTE) && \
+    (H2V_HPACK_HUFFMAN_DECODER_USE_FULLBYTE == 1)
+extern "C" inline const char H2V_HPACK_HUFFMAN_DECODER[]
+    __attribute__((used, visibility("default"))) =
+        "H2V_HPACK_HUFFMAN_DECODER: 0x1 - Precomputed Full-Byte FSM";
+#else
+extern "C" inline const char H2V_HPACK_HUFFMAN_DECODER[]
+    __attribute__((used, visibility("default"))) =
+        "H2V_HPACK_HUFFMAN_DECODER: 0x0 - Precomputed 4Bit Nibble FSM";
+#endif
+
 namespace h2v {
 namespace hpack {
 
 using HpackErrorCode = int32_t;
+
 namespace HPACK_ERR {
 
 static constexpr HpackErrorCode NONE = 0;
